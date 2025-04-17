df04828f815171976704093c40c8a81f60da1e8a
Aryan-kamal <kamalaryan1234567@gmail.com>
2025-04-17 23:51:38 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-export const serverUrl="http://localhost:400/api"
+export const serverUrl = "https://petadoptionbackend-production.up.railway.app/api";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
