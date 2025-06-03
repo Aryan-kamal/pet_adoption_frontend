f1fa1b7efaf7bdaca336da1fcff11657d63cf998
Aryan-kamal <kamalaryan1234567@gmail.com>
2025-06-03 23:39:36 +0530

    newbackend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-export const serverUrl = "http://localhost:4000/api";
+export const serverUrl = "petadoptionbackend-production.up.railway.app";
 //  http://localhost:4000/api
 // https://petadoptionbackend-production.up.railway.app/api
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
